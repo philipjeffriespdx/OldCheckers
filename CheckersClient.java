0d752104e87dfcd59fb47ddf70754f87f46e2b7f
Philip Jeffries <philipjeffriespdx@pointloma.edu>
2017-11-04 16:44:20 -0700

    First commit

---
+++ CheckersClient.java
@@ -0,0 +1,30 @@
+/*
+Philip Jeffries
+Networking and Security 
+Checkers Game
+*/
+import java.io.*;
+import java.net.*;
+
+//There will be a checkers Server and Client
+//Server Starts the Game and Client connects
+class CheckersClient {
+   public static void main(String argv[]) throws Exception {
+   //Connect to Checkers Server 
+   
+   //Create Matrix of Peices
+   
+   //Create GUI of board
+   
+   //LOOP OF MOVES ::
+   //Wait for Server move
+   //Update Matrix
+   //Repaint
+   //Ask user for move (Have user type moves Ex: A1 to B2
+   //Tell user if move is valid
+   //Change Matrix value
+   //Send valid move to other player
+   //Repaint
+   
+   }
+}
---
+++ CheckersServer.java
@@ -0,0 +1,39 @@
+/*
+Philip Jeffries
+Networking and Security 
+Checkers Game
+*/
+import java.io.*;
+import java.net.*;
+
+//There will be a checkers Server and Client
+//Server Starts the Game
+class CheckersServer {
+   public static void main(String argv[]) throws Exception {
+   //Make connection with Checkers Client 
+   
+   //Create Matrix of Peices
+   
+   //Create GUI of board
+   
+   
+   //FIRST SERVER MOVE
+   //Ask user for move (Have user type moves Ex: A1 to B2
+   //Tell user if move is valid
+   //Change Matrix value
+   //Send valid move to other player
+   //Repaint
+   
+   
+   //LOOP OF MOVES ::
+   //Wait for Client move
+   //Update Matrix
+   //Repaint
+   //Ask user for move (Have user type moves Ex: A1 to B2
+   //Tell user if move is valid
+   //Change Matrix value
+   //Send valid move to other player
+   //Repaint
+   
+   }
+}
